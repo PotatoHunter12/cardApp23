7ad6a11c50e2e575bbda7e6736e23b321067c8e5
Ambrož Perovšek <64433302+PotatoHunter12@users.noreply.github.com>
2023-10-24 11:23:55 +0200

    rules added

---
+++ src/views/TarokRulesView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Tarok rules</h1>
+    
+    <p>The Tarok pack has 54 cards; it is essentially the same as the pack used for Tarock in Austria, 
+        and for Taroky in the Czech Republic and Slovakia. There are 8 cards in each of the four suits Clubs, 
+        Spades, Hearts and Diamonds, plus 22 trumps (taroks). The highest tarok, the Škis (pronounced shkiss), 
+        looks rather like a Joker. The second highest trump (XXI) is called the Mond and the lowest trump (I) 
+        is called the Pagat. The black suits rank from highest to lowest: King, Queen, Knight, Jack, 10, 9, 8, 7. 
+        The red suits rank from highest to lowest: King, Queen, Knight, Jack 1, 2, 3, 4.
+    </p>
+    <p>The taroks all have large roman numerals except for the škis. In the four suits, 
+        the king has a crown, the queen is female, the knight rides a horse, and the jack is 
+        the other picture card. The numeral cards are identified by counting the spots on them 
+        (there is no corner index).
+    </p>
+    <p>In each hand the declarer is the player who is prepared to undertake the highest contract. 
+        This is decided by bidding, so in order to understand the bidding, it is first necessary to know 
+        what contracts are possible. The available contracts are listed below in ascending order, with their 
+        scores and brief descriptions. Further details of the contracts are given later.
+    </p>
+    <h3>The bids</h3>
+    <p><strong>klop</strong> - minus (points taken) or 70</p>
+    <p><strong>three</strong> - 10 + difference</p>
+    <p><strong>two</strong> - 20 + difference</p>
+    <p><strong>one</strong> - 30 + difference</p>
+    <p><strong>solo three</strong> - 40 + difference</p>
+    <p><strong>solo two</strong> - 50 + difference</p>
+    <p><strong>solo one</strong> - 60 + difference</p>
+    <p><strong>beggar</strong> - 70</p>
+    <p><strong>solo without</strong> - 80</p>
+    <p><strong>colour valat without </strong> - 125</p>
+    <p><strong>valat without </strong> - 500</p>
+    
+</template>
+
+<style>
+
+</style>
