670e68eb0cf2d72529123e1f22ebbbb6a43af737
Ambrož Perovšek <64433302+PotatoHunter12@users.noreply.github.com>
2023-10-30 15:51:34 +0100

    Update Tarok game view

--- src/views/TarokGameView.vue
+++ src/views/TarokGameView.vue
@@ -3,10 +3,63 @@
 
 <template>
   <main>
-    <h1>tarok</h1>
+    <table>
+      <tr>
+        <td>
+          ime 1
+        </td>
+        <td>
+          ime 2
+        </td>
+        <td>
+          ime 3
+        </td>
+        <td>
+          ime 4
+        </td>
+      </tr>
+      <tr class="rad">
+        <td>
+          o
+        </td>
+        <td>
+          o
+        </td>
+        <td>
+          o
+        </td>
+        <td>
+          o
+        </td>
+      </tr>
+      <tr>
+        <td>
+          0
+        </td>
+        <td>
+          0
+        </td>
+        <td>
+          0
+        </td>
+        <td>
+          0
+        </td>
+      </tr>
+    </table>
   </main>
 </template>
 
 <style>
+td {
+  border-bottom: 2pt black solid;
+  
+}
+  table {
+    border-collapse: collapse;
+    width: 100%;
+    margin: 0;
+    padding: 0;
 
+  }
 </style>
